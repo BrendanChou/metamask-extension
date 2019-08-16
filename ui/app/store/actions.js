aba4318a7cae856f53cff97fa63a3a2d1ba15d8c
Dan Miller <danjm.com@gmail.com>
2019-08-16 01:01:13 -0230

    Remove unnecessary method calls after restoration from 3box in the threebox-restore-confirm modal.

--- ui/app/store/actions.js
+++ ui/app/store/actions.js
@@ -387,6 +387,7 @@ var actions = {
   getThreeBoxLastUpdated,
   setThreeBoxSyncingPermission,
   setRestoredFromThreeBox,
+  turnThreeBoxSyncingOn,
 }
 
 module.exports = actions
@@ -2848,6 +2849,20 @@ function setRestoredFromThreeBox (restored) {
   return (dispatch) => {
     return new Promise((resolve, reject) => {
       background.setRestoredFromThreeBox(restored, (err) => {
+        if (err) {
+          dispatch(actions.displayWarning(err.message))
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  }
+}
+
+function turnThreeBoxSyncingOn {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      background.turnThreeBoxSyncingOn((err) => {
         if (err) {
           dispatch(actions.displayWarning(err.message))
           return reject(err)
